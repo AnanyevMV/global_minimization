f2434515548caa51691c0ecc98a9b95ba6dd7508
Michido <michido@protonmail.com>
2019-11-01 21:09:53 +0300

    Added threads pool

--- repos_5_worldfly_minimization/src/global_min.cpp
+++ repos_5_worldfly_minimization/src/global_min.cpp
@@ -1,197 +1,191 @@
 #include "global_min.hpp"
-#include <thread>
-#include <mutex>
 
-// Автор: Козырев Дмитрий
+#include <array>
+#include "thread_pool.h"
+
 std::vector<std::pair<Real, Vector>>
-calc_f_with_threads(Function f, const std::vector<Vector> & inData) {
-	// Создаем вектор под ответ:
-	std::vector<std::pair<Real, Vector>> outData(inData.size());
-	
-	// Количество ядер:
-	uint32_t nCores = std::max(1u, std::thread::hardware_concurrency());
-	
-	// Вектор из тредов:
-	std::vector<std::thread> t;
-	
-	// Мьютексы на чтение и запись:
-	std::mutex inRead, outWrite;
-	
-	uint32_t globalIndex = 0;
-	
-	// Создаем столько тредов, сколько ядер:
-	for (uint32_t i = 0; i < nCores; i++) {
-		t.push_back(std::thread([&] {
-			while (1) {
-				inRead.lock();
-				uint32_t i = globalIndex;
-				if (globalIndex >= inData.size()) {
-					inRead.unlock();
-					return;
-				}
-				
-				auto x = inData[i];
-				globalIndex++;
-				inRead.unlock();
+calc_f_with_threads(Function f, const std::vector<Vector>& inData)
+{
+    // Создаем вектор под ответ:
+    std::vector<std::pair<Real, Vector>> outData(inData.size());
 
-				auto res = f(x);
+    // Количество ядер:
+    uint32_t nCores = std::max(1u, std::thread::hardware_concurrency());
 
-				outWrite.lock();
-				outData[i] = {res, x};
-				outWrite.unlock();
-			}
-			return;
-		}));
-	}
-	
-	// Присоединяем все треды к материнскому треду, гарантируя то, что все они будут выполнены
-	for (uint32_t i = 0; i < nCores; ++i) {
-		t[i].join();
-	}
-	
-	assert(globalIndex == inData.size());
-	
-	return outData;
+    // Вектор из тредов:
+    std::vector<std::thread> t;
+
+    // Мьютексы на чтение и запись:
+    std::mutex inRead, outWrite;
+
+    uint32_t globalIndex = 0;
+
+    // Создаем столько тредов, сколько ядер:
+    for (uint32_t i = 0; i < nCores; i++)
+    {
+        t.push_back(std::thread([&]
+        {
+            while (1)
+            {
+                inRead.lock();
+                uint32_t i = globalIndex;
+                if (globalIndex >= inData.size())
+                {
+                    inRead.unlock();
+                    return;
+                }
+
+                auto x = inData[i];
+                globalIndex++;
+                inRead.unlock();
+
+                auto res = f(x);
+
+                outWrite.lock();
+                outData[i] = {res, x};
+                outWrite.unlock();
+            }
+            return;
+        }));
+    }
+
+    // Присоединяем все треды к материнскому треду, гарантируя то, что все они будут выполнены
+    for (uint32_t i = 0; i < nCores; ++i)
+    {
+        t[i].join();
+    }
+
+    assert(globalIndex == inData.size());
+
+    return outData;
 }
 
-// Автор: Козырев Дмитрий
 std::vector<std::pair<Real, Vector>>
-find_local_mins_with_threads(Function f, const StopCondition& stop_condition, const std::vector<std::pair<Real, Vector>>& inData) {
-	// Создаем вектор под ответ:
-	std::vector<std::pair<Real, Vector>> outData(inData.size());
-	
-	// Количество ядер:
-	uint32_t nCores = std::max(1u, std::thread::hardware_concurrency());
-	
-	// Вектор из тредов:
-	std::vector<std::thread> t;
-    
-	// Мьютексы на чтение и запись:
-	std::mutex inRead, outWrite;
-	
-	uint32_t globalIndex = 0;
-	
-	// Создаем столько тредов, сколько ядер:
-	for (uint32_t thread_id = 0; thread_id < nCores; thread_id++) {
-		t.push_back(std::thread([&] {
-			while (1) {
-				inRead.lock();
-				uint32_t i = globalIndex;
-				if (globalIndex >= inData.size()) {
-					inRead.unlock();
-					return;
-				}
-				
-				auto it = inData[i];
-				globalIndex++;
-				inRead.unlock();
-				
-				// После чтения вызываем методы минимизации:
-				auto iter_data = bfgs(f, it.second, stop_condition);
-                auto x1 = iter_data.x_curr;
-				auto f1 = iter_data.f_curr;
-                
-                iter_data = hessian_free(f, it.second, stop_condition);
-				auto x2 = iter_data.x_curr;
-                auto f2 = iter_data.f_curr;
-                
-                iter_data = nesterov(f, it.second, stop_condition);
-				auto x3 = iter_data.x_curr;
-                auto f3 = iter_data.f_curr;
-                
-                iter_data = dfp(f, it.second, stop_condition);
-                auto x4 = iter_data.x_curr;
-                auto f4 = iter_data.f_curr;
-                
-                iter_data = powell(f, it.second, stop_condition);
-                auto x5 = iter_data.x_curr;
-                auto f5 = iter_data.f_curr;
-                
-				// Записываем ответ:
-				outWrite.lock();
-				outData[i] = std::min({
-					it,
-					std::make_pair(f1, x1),
-					std::make_pair(f2, x2), 
-					std::make_pair(f3, x3), 
-                    std::make_pair(f4, x4),
-                    std::make_pair(f5, x5) 
-				});
-				outWrite.unlock();
-			}
-			return;
-		}));
-	}
-	
-	// Присоединяем все треды к материнскому треду, гарантируя то, что все они будут выполнены
-	for (uint32_t i = 0; i < nCores; ++i) {
-		t[i].join();
-	}
-    
-	assert(globalIndex == inData.size());
-	
-	return outData;
-	
+find_local_mins_with_threads(Function f, const StopCondition& stop_condition,
+                             const std::vector<std::pair<Real, Vector>>& inData)
+{
+    // Создаем вектор под ответ
+    std::vector<std::pair<Real, Vector>> outData(inData.size());
+
+    // Количество потоков
+    const uint32_t cores = ThreadPool::GetHardwareCoresAmount();
+
+    // Создаем пул потоков
+    ThreadPool thread_pool{cores};
+
+    // Массив функций минимизации
+    static const std::array<std::function<IterationData(Function, Vector, const StopCondition&)>, 6> min_funcs{
+        &bfgs, &hessian_free, &slow_hessian_free, &nesterov, &dfp, &powell
+    };
+
+    // Мьютекс на запись
+    pthread_mutex_t outWrite;
+    pthread_mutex_init(&outWrite, nullptr);
+
+    for (size_t i = 0; i < inData.size(); ++i)
+    {
+        const auto& data = inData[i];
+
+        // Записываем начальное значение
+        outData[i] = data;
+
+        for (const auto& min_func : min_funcs)
+        {
+            thread_pool.Add([&, data, i]()
+            {
+                const auto iter_data = min_func(f, data.second, stop_condition);
+                const auto x_curr = iter_data.x_curr;
+                const auto f_curr = iter_data.f_curr;
+
+                const auto pair = std::make_pair(f_curr, x_curr);
+
+                // Записываем ответ
+                pthread_mutex_lock(&outWrite);
+
+                // Заменяемым минимальным
+                outData[i] = std::min(outData[i], pair);
+
+                pthread_mutex_unlock(&outWrite);
+            });
+        }
+    }
+
+    thread_pool.CreateThreads();
+    thread_pool.WaitForAll();
+
+    return outData;
 }
 
-// Автор: Козырев Дмитрий
 std::vector<std::pair<Real, Vector>>
-find_absmin(Function f, const StopCondition& stop_condition, uint32_t dim, uint32_t nBestPoints, uint32_t nAllPoints, Vector min, Vector max) {
-	// Несколько проверок на входные данные:
-	assert(dim > 0u && dim == min.size() && dim == max.size());
-	assert(nBestPoints <= nAllPoints && nBestPoints > 0u);
-	for (uint32_t i = 0; i < dim; ++i) {
-		assert(min[i] <= max[i]);
-	}
-	
-	// Объект-генератор сетки:
-	SobolSeqGenerator net;
-	net.Init(nAllPoints, dim, "new-joe-kuo-6.21201.txt");
-	
-	// ----- Первый этап: вычисление значений функции в узлах сетки с отбором точек-кандидатов -----
-	
-	// Лимит на группу из одновременно обрабатываемых точек:
-	const uint32_t GROUP_SIZE = 1024u;
-	
-	// Формирование списка лучших кандидатов
-	std::set<std::pair<Real, Vector>> candidates;
-	
-	// Сначала складываем точки группами по GROUP_SIZE:
-	std::vector<Vector> group;
-	for (uint32_t i = 0; i < nAllPoints; ++i) {
-		// Получение текущего узла сетки:
-		const auto net_point = net.GeneratePoint().coordinate;
-		// Преобразование узла к точке в ограниченной min и max области:
-		Vector curr(dim);
-		for (uint32_t i = 0; i < dim; ++i) {
-			curr[i] = net_point[i] * (max[i] - min[i]) + min[i];
-		}
-		if (i == nAllPoints - 1 || group.size() == GROUP_SIZE) {
-			// Запускаем многопоточное вычисление значений во всех точках
-			for (auto& it : calc_f_with_threads(f, group)) {
-				candidates.insert(it);
-				if (candidates.size() > nBestPoints) {
-					candidates.erase(std::prev(candidates.end()));
-				}
-			}
-			group.clear();
-		} else {
-			group.push_back(curr);
-		}
-	}
-	
-	// ----- Второй этап: запуск алгоритмов поиска локального минимума из выбранных точек -----
-	// Подготовка (перекладываем точки из set в vector - возможен рост скорости при последовательном размещении в памяти точек):
-	std::vector<std::pair<Real, Vector>> temp;
-	for (auto & it : candidates) {
-		temp.push_back(it);
-	}
-	
-	// Многопоточная обработка кандидатов:
-	auto answer = find_local_mins_with_threads(f, stop_condition, temp);
-	
-	// Итоговая сортировка всех найденных точек по неубыванию значения функции в них:
-	std::sort(answer.begin(), answer.end());
-	
-	return answer;
+find_absmin(Function f, const StopCondition& stop_condition, uint32_t dim, uint32_t nBestPoints, uint32_t nAllPoints,
+            Vector min, Vector max)
+{
+    // Несколько проверок на входные данные:
+    assert(dim > 0u && dim == min.size() && dim == max.size());
+    assert(nBestPoints <= nAllPoints && nBestPoints > 0u);
+    for (uint32_t i = 0; i < dim; ++i)
+    {
+        assert(min[i] <= max[i]);
+    }
+
+    // Объект-генератор сетки:
+    SobolSeqGenerator net;
+    //net.Init(nAllPoints, dim, "D:\\Programs\\VS\\worldfly_minimization\\WorldflyMinimization\\bin\\tests\\new-joe-kuo-6.21201.txt");
+    net.Init(nAllPoints, dim,
+             "D:\\Programs\\VS\\worldfly_minimization\\WorldflyMinimization\\bin\\tests\\new-joe-kuo-6.21201.txt");
+
+    // ----- Первый этап: вычисление значений функции в узлах сетки с отбором точек-кандидатов -----
+
+    // Лимит на группу из одновременно обрабатываемых точек:
+    const uint32_t GROUP_SIZE = 1024u;
+
+    // Формирование списка лучших кандидатов
+    std::set<std::pair<Real, Vector>> candidates;
+
+    // Сначала складываем точки группами по GROUP_SIZE:
+    std::vector<Vector> group;
+    for (uint32_t i = 0; i < nAllPoints; ++i)
+    {
+        // Получение текущего узла сетки:
+        const auto net_point = net.GeneratePoint().coordinate;
+        // Преобразование узла к точке в ограниченной min и max области:
+        Vector curr(dim);
+        for (uint32_t i = 0; i < dim; ++i)
+        {
+            curr[i] = net_point[i] * (max[i] - min[i]) + min[i];
+        }
+        if (i == nAllPoints - 1 || group.size() == GROUP_SIZE)
+        {
+            // Запускаем многопоточное вычисление значений во всех точках
+            for (auto& it : calc_f_with_threads(f, group))
+            {
+                candidates.insert(it);
+                if (candidates.size() > nBestPoints)
+                {
+                    candidates.erase(std::prev(candidates.end()));
+                }
+            }
+            group.clear();
+        }
+        else
+        {
+            group.push_back(curr);
+        }
+    }
+
+    // ----- Второй этап: запуск алгоритмов поиска локального минимума из выбранных точек -----
+    // Подготовка (перекладываем точки из set в vector - возможен рост скорости при последовательном размещении в памяти точек):
+    std::vector<std::pair<Real, Vector>> temp;
+    for (auto& it : candidates)
+    {
+        temp.push_back(it);
+    }
+
+    // Многопоточная обработка кандидатов:
+    auto answer = find_local_mins_with_threads(f, stop_condition, temp);
+
+    // Итоговая сортировка всех найденных точек по неубыванию значения функции в них:
+    std::sort(answer.begin(), answer.end());
+
+    return answer;
 }
---
+++ repos_5_worldfly_minimization/src/thread_pool.cpp
@@ -0,0 +1,72 @@
+#include "thread_pool.h"
+
+#include <thread>
+#include <algorithm>
+
+ThreadPool::ThreadPool(size_t threads)
+    : threads_amount_(threads)
+{
+    pthread_mutex_init(&queue_mutex_, nullptr);
+}
+
+void ThreadPool::CreateThreads()
+{
+    for (size_t i = 0; i < threads_amount_; ++i)
+    {
+        pthread_t thr;
+        pthread_create(&thr, nullptr, &ThreadPool::WorkerThreadProc, static_cast<void*>(this));
+
+        workers_.push_back(thr);
+    }
+}
+
+void* ThreadPool::WorkerThreadProc(void* object)
+{
+    ThreadPool* thread_pool = static_cast<ThreadPool*>(object);
+
+    for (;;)
+    {
+        pthread_mutex_lock(&thread_pool->queue_mutex_);
+
+        if (thread_pool->tasks_.empty())
+        {
+            pthread_mutex_unlock(&thread_pool->queue_mutex_);
+            break;
+        }
+
+        std::function<void()> task = std::move(thread_pool->tasks_.front());
+
+        thread_pool->tasks_.pop();
+
+        pthread_mutex_unlock(&thread_pool->queue_mutex_);
+
+        task();
+    }
+
+    pthread_exit(nullptr);
+
+    return nullptr;
+}
+
+bool ThreadPool::Add(const std::function<void()>& func)
+{
+    pthread_mutex_lock(&queue_mutex_);
+
+    tasks_.push(func);
+
+    pthread_mutex_unlock(&queue_mutex_);
+
+    return true;
+}
+
+void ThreadPool::WaitForAll()
+{
+    for (pthread_t worker : workers_)
+        pthread_join(worker, nullptr);
+}
+
+uint32_t ThreadPool::GetHardwareCoresAmount()
+{
+    const uint32_t cores = std::max(1u, std::thread::hardware_concurrency());
+    return cores > 4 ? cores - 1 : cores;
+}
